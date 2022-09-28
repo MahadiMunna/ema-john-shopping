b87bce85aa7aade582697f92c7f4bccc95a3c672
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-28 18:52:20 +0600

    new component cart added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = () => {
     return (
         <nav className='header'>
-            <img src={logo}/>
+            <img src={logo} alt="" />
             <div>
                 <a href="/shop">Shop</a>
                 <a href="/orders">Orders</a>
